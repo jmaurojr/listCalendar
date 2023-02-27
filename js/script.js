bbe75a3dfa44407155ed3e23aea3828ff49bb116
jmaurojr <112908979+jmaurojr@users.noreply.github.com>
2023-02-26 21:13:23 -0300

    adding initial JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-dycalendar.draw({
-  target: "#dycalendar",
-});
+const currentDate = document.querySelector(".current-date");
+
+let date = new Date(),
+  currYear = date.getFullYear(),
+  currMonth = date.getMonth();
+
+/* console.log(date, currYear, currMonth); */
+const renderCalendar = () => {
+  currentDate.innerText = `${currMonth} ${currYear}`;
+};
